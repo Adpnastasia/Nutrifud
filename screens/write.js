62daa6d245a327ca7b1a358a5813ff5b0366f641
gincuumaybe <christherber445@gmail.com>
2023-11-07 14:30:41 +0700

    pengerjaan fitur Write

--- screens/write.js
+++ screens/write.js
@@ -1,16 +1,68 @@
-import { Heading, Center } from "native-base";
+import React from "react";
+import {
+  Input,
+  Stack,
+  TextArea,
+  FormControl,
+  View,
+  Text,
+  Button,
+  Container,
+  Center,
+  Box,
+  Flex,
+  Spacer,
+} from "native-base";
+import { useNavigation } from "@react-navigation/native";
 
-const Write = () => {
+export const FormExample = () => {
   return (
-    <>
-      <Center 
-        flex={1}
-        bg="#FFEEDC"
+    <Box
+      flex={1}
+      p={4}
+      alignItems={"center"}
+      justifyContent={"center"}
+      bg={"white"}
+    >
+      <FormControl alignItems={"center"}>
+        <Stack space={10}>
+          <Stack>
+            <FormControl.Label>Nama</FormControl.Label>
+            <Input variant="underlined" p={2} placeholder="Nama Resep" />
+          </Stack>
+          <Stack>
+            <FormControl.Label>Bahan</FormControl.Label>
+            <TextArea
+              h={20}
+              placeholder="Bahan - bahan yang diperlukan"
+              w="100%"
+              maxW="100%"
+            />
+          </Stack>
+          <Stack>
+            <FormControl.Label>Cara Pembuatan</FormControl.Label>
+            <TextArea
+              h={20}
+              placeholder="Cara pembuatan"
+              w="100%"
+              maxW="100%"
+            />
+          </Stack>
+        </Stack>
+      </FormControl>
+
+      <Button
+        bg={"#FFEEDC"}
+        m={"10"}
+        borderRadius={"lg"}
+        size={"lg"}
+        maxHeight={"full"}
+        maxWidth={"full"}
       >
-        <Heading>Write</Heading>
-      </Center>
-    </>
+        <Text>Kirim</Text>
+      </Button>
+    </Box>
   );
 };
 
-export default Write;
+export default FormExample;
